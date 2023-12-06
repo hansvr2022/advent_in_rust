ad8ab9c3d967900ab6d42def83d776294dd13d90
hans.vanroon <hans.vanroon@nedap.com>
2023-12-06 15:57:53 +0100

    optimized day 5

--- src/day_five.rs
+++ src/day_five.rs
@@ -1,22 +1,24 @@
 #[cfg(test)]
 mod day_five {
+    use std::cmp::{max, min};
     use std::collections::HashMap;
+    use std::ops::Range;
     use std::str::FromStr;
     use rayon::iter::*;
 
     #[test]
     fn test() {
         let lines = include_str!("../resources/input_day_five.txt").lines();
-        let mut seeds: Vec<Vec<u64>> = Vec::new();
+        let mut seeds: Vec<Range<i64>> = Vec::new();
         let mut destination_map = "seed to soil";
-        let mut all_the_mappers: HashMap<String, HashMap<u64, u64>> = HashMap::new();
-        all_the_mappers.insert(String::from("seed-to-soil"), HashMap::new());
-        all_the_mappers.insert(String::from("soil-to-fertilizer"), HashMap::new());
-        all_the_mappers.insert(String::from("fertilizer-to-water"), HashMap::new());
-        all_the_mappers.insert(String::from("water-to-light"), HashMap::new());
-        all_the_mappers.insert(String::from("light-to-temperature"), HashMap::new());
-        all_the_mappers.insert(String::from("temperature-to-humidity"), HashMap::new());
-        all_the_mappers.insert(String::from("humidity-to-location"), HashMap::new());
+        let mut all_the_mappers: HashMap<String, Vec<Mapper>> = HashMap::new();
+        all_the_mappers.insert(String::from("seed-to-soil"), Vec::new());
+        all_the_mappers.insert(String::from("soil-to-fertilizer"), Vec::new());
+        all_the_mappers.insert(String::from("fertilizer-to-water"), Vec::new());
+        all_the_mappers.insert(String::from("water-to-light"), Vec::new());
+        all_the_mappers.insert(String::from("light-to-temperature"), Vec::new());
+        all_the_mappers.insert(String::from("temperature-to-humidity"), Vec::new());
+        all_the_mappers.insert(String::from("humidity-to-location"), Vec::new());
         let keys = vec!["seed-to-soil",
                         "soil-to-fertilizer",
                         "fertilizer-to-water",
@@ -28,17 +30,15 @@ mod day_five {
 
         for line in lines {
             if line.starts_with("seeds:") {
-                let seeds_and_range: Vec<u64> = line.split(":").last().unwrap()
-                    .trim().split(" ").map(|x| u64::from_str(x).unwrap()).collect();
-                let mut seeds_after_range: Vec<Vec<u64>> = Vec::new();
+                let seeds_and_range: Vec<i64> = line.split(":").last().unwrap()
+                    .trim().split(" ").map(|x| i64::from_str(x).unwrap()).collect();
                 for i in 0..seeds_and_range.len() {
                     if i % 2 == 0 {
-                        let to = seeds_and_range[i] + seeds_and_range[i + 1] - 1;
-                        let seeds_to_add: Vec<u64> = (seeds_and_range[i]..to).collect();
-                        seeds_after_range.push(seeds_to_add);
+                        let to: i64 = seeds_and_range[i] + seeds_and_range[i + 1];
+                        let seeds_to_add = seeds_and_range[i]..to;
+                        seeds.push(seeds_to_add);
                     }
                 }
-                seeds = seeds_after_range;
             } else if line.starts_with("seed-to-soil") {
                 destination_map = "seed-to-soil";
             } else if line.starts_with("soil-to-fertilizer") {
@@ -54,36 +54,56 @@ mod day_five {
             } else if line.starts_with("humidity-to-location") {
                 destination_map = "humidity-to-location";
             } else if line.is_empty() {} else {
-                let numbers: Vec<_> = line.trim().split(" ").filter(|x| !x.is_empty()).map(|x| u64::from_str(x).unwrap()).collect();
-                let mut hash: HashMap<u64, u64> = HashMap::new();
-                for i in 0..(numbers[2] - 1) {
-                    hash.insert(numbers[1] + i, numbers[0] + i);
-                }
-                if all_the_mappers.contains_key(destination_map) {
-                    all_the_mappers.get_mut(destination_map).unwrap().extend(hash);
-                } else {
-                    all_the_mappers.insert(destination_map.parse().unwrap(), hash);
-                }
+                let numbers: Vec<_> = line.trim().split(" ").filter(|x| !x.is_empty()).map(|x| i64::from_str(x).unwrap()).collect();
+                all_the_mappers.get_mut(destination_map).unwrap().push(Mapper { delta: numbers[1] - numbers[0], range: numbers[1]..(numbers[1] + numbers[2]) });
             }
         }
 
+        println!("seeds: {:?}", seeds);
+        let ranges: Vec<_> = all_the_mappers.values().map(|x| x.first().unwrap()).map(|x| x.range.start).collect();
+        let deltas: Vec<_> = all_the_mappers.values().map(|x| x.first().unwrap()).map(|x| x.delta).zip(ranges).collect();
+        println!("mappers: {:?}", deltas);
 
-        println!("start with this amount of seeds: {}", seeds.len());
-        let lowest = seeds.par_iter().map(|seeds| {
-            seeds.par_iter().map(|seed| {
-                let mut current_source = *seed;
-                for key in &keys {
-                    current_source = *all_the_mappers.get(*key).unwrap().get(&current_source).unwrap_or(&current_source);
+        let results: Vec<_> = seeds.iter()
+            .map(|x| whoop(x, &all_the_mappers, "seed-to-soil")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "soil-to-fertilizer")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "fertilizer-to-water")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "water-to-light")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "light-to-temperature")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "temperature-to-humidity")).flatten()
+            .map(|x| whoop(&x, &all_the_mappers, "humidity-to-location")).flatten().collect();
+
+        let smallest = results.iter().map(|t: &Range<i64>| t.start).min();
+
+
+        println!("lowest: {:?}", smallest);
+    }
+
+    fn whoop(range: &Range<i64>, all_the_mappers: &HashMap<String, Vec<Mapper>>, key: &str) -> Vec<Range<i64>> {
+        let start = range.start;
+        let end = range.end;
+        let mut result: Vec<_> = all_the_mappers.get(key).unwrap().iter()
+            .filter(move |m| start <= m.range.end && end >= m.range.start)
+            .map(move |m| {
+                let new_range = (max(start, m.range.start) - m.delta)..(min(end, m.range.end) - m.delta);
+                if(start == 0) {
+                    println!("new range: {}-{}, delta: {}", new_range.start, new_range.end, m.delta)
                 }
-                return current_source;
-            }).min()
-        }).min();
+                return new_range;
+            }).collect();
+        let min_result = result.iter().map(|x| x.start).min();
+        let max_result = result.iter().map(|x| x.end).max();
+        if result.is_empty() {
+            return vec![start..end]
+        }
 
-        println!("lowest: {}", lowest.unwrap().unwrap());
+        return result;
     }
 
     struct Mapper {
-        is_for: String,
-        swap: HashMap<u64, u64>,
+        delta: i64,
+        range: Range<i64>,
     }
+
+    impl Mapper {}
 }
